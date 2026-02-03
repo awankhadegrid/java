ae6d14c4af8d420eaf59be2cdafb5dbc2b5027fa
awankhadegrid <awankhade@griddynamics.com>
2026-02-03 15:36:19 +0530

    exception handling

---
+++ Topics/Throwing exceptions/Throw an unchecked exception/src/Main.java
@@ -0,0 +1,20 @@
+import java.io.UncheckedIOException;
+
+public class Main {
+
+    public static void method() throws Exception {
+        Exception e=new RuntimeException();
+        throw e;
+    }
+
+    /* Do not change code below */
+    public static void main(String[] args) {
+        try {
+            method();
+        } catch (RuntimeException e) {
+            System.out.println("RuntimeException");
+        } catch (Exception e) {
+            System.out.println("Exception");
+        }
+    }
+}
